4a27aed2858c8fa4be913930394733bd9f2290c3
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-04-14 19:52:26 +0700

    belajar tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+// Jika tidak berencana menulis custom cSS apapun
+// import 'tailwindcss/tailwind.css';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
