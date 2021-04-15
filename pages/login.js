6e35f67c4394fe0bb40912ff76fea3450d0b5761
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-04-15 10:58:41 +0700

    login page done: html css

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import styles from "@/styles/register.module.css";
 import clsx from "clsx";
-import Link from 'next/link';
+import Link from "next/link";
 import InputIcon from "@/components/inputs/InputIcon";
 import { MailIcon } from "@heroicons/react/solid";
 import { LockClosedIcon } from "@heroicons/react/solid";
@@ -13,17 +13,9 @@ export default function Register() {
   return (
     <section className="container mx-auto py-20">
       <div className={clsx(styles.wrapper, "mx-auto")}>
-        <div
-          className="border border-gray-light rounded-3xl px-14 py-12 mb-4"
-        >
+        <div className="border border-gray-light rounded-3xl px-14 py-12 mb-4">
           <img src="/images/devchallenges.svg" alt="Logo" className="pb-6" />
-          <h1 className="text-lg font-semibold pb-4">
-            Join thousands of learners from around the world{" "}
-          </h1>
-          <p className="pb-6">
-            Master web development by making real-life projects. There are
-            multiple paths for you to choose
-          </p>
+          <h1 className="text-lg font-semibold mb-6">Login </h1>
 
           <form action="" className="mb-8">
             <InputIcon
@@ -45,7 +37,7 @@ export default function Register() {
               type="submit"
               className="py-2 text-white bg-blue rounded-md w-full font-semibold"
             >
-              Register
+              Login
             </button>
           </form>
           <small className="text-center text-sm text-gray w-full block mb-6">
@@ -68,16 +60,17 @@ export default function Register() {
           </div>
 
           <small className="block text-center text-sm text-gray w-full">
-            Adready a member?{" "}
-            <Link href="login">
-              <a className="text-blue-light">
-                Login
-              </a>
+            Don't have an account yet?{" "}
+            <Link href="register">
+              <a className="text-blue-light">Register</a>
             </Link>
           </small>
         </div>
         <div className="text-gray text-sm flex">
-          <span className="inline-block mr-auto">created by <strong className="underline">Mohamad Ilham Ramadhan</strong></span>
+          <span className="inline-block mr-auto">
+            created by{" "}
+            <strong className="underline">Mohamad Ilham Ramadhan</strong>
+          </span>
           <span className="inline-block ml-auto">devChallenges.io</span>
         </div>
       </div>
